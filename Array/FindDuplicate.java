e137016ac0fdc061121680cf1efb60f9d373f371
aieman <aqsiddique@gmail.com>
2019-09-29 10:22:54 +1000

    Added comments

--- Array/FindDuplicate.java
+++ Array/FindDuplicate.java
@@ -11,10 +11,13 @@ public class FindDuplicate {
         HashSet<Integer> set = new HashSet<Integer>();
 
         for(int i = 0; i < data.length - 1; i++){
-            if(data[i] == data [i+1])
+            // if a duplicate is found then adding it to the set, 
+            // even if there are multiple the set will contain just keep 1 element of it as set doesn't keep duplicates
+            if(data[i] == data [i+1]) 
                 set.add(data[i]);
         }
 
+        // printing all the values in the set which will have the duplicated numbers
         for(Integer val : set){
             System.out.println(val);
         }
